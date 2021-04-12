6159ac71525e398fc4edb0adafbc750faaec2e14
dengxuhui <419270388@qq.com>
2021-04-12 11:37:43 +0800

    文件拷贝bug

--- MultiLanguage/Assets/Editor/MultiLanguage/Scripts/func/FuncCopyAssets.cs
+++ MultiLanguage/Assets/Editor/MultiLanguage/Scripts/func/FuncCopyAssets.cs
@@ -1,5 +1,7 @@
 ﻿using System.IO;
 using MultiLanguage.Scripts.tool;
+using UnityEditor;
+using UnityEngine;
 using Config = MultiLanguage.Scripts.MultiLanguageConfig;
 
 namespace MultiLanguage.Scripts.func
@@ -13,13 +15,20 @@ namespace MultiLanguage.Scripts.func
         {
             var rules = MultiLanguageAssetsManager.GetRules();
             var supports = rules.supports;
+            var tDir = Path.GetDirectoryName(Application.dataPath);
+            if (string.IsNullOrEmpty(tDir))
+            {
+                return;
+            }
+
+            tDir = Path.Combine(tDir, rules.runtimeAssetsDirectory);
             for (var i = 0; i < supports.Length; i++)
             {
                 var support = supports[i];
                 var abbr = string.IsNullOrEmpty(support.abbr) ? support.language.ToString() : support.abbr;
                 var name = string.Format(MultiLanguageConfig.BuildLanguageFormat, abbr);
                 var srcPath = Path.Combine(FileTool.GetFullPath(rules.buildDirectory), name);
-                var targetPath = Path.Combine(FileTool.GetFullPath(rules.runtimeAssetsDirectory), name);
+                var targetPath = Path.Combine(tDir, name);
                 if (!File.Exists(srcPath))
                 {
                     continue;
@@ -29,7 +38,10 @@ namespace MultiLanguage.Scripts.func
                 {
                     File.Delete(targetPath);
                 }
+                FileTool.TryMakeDir(Path.GetDirectoryName(targetPath));
                 File.Copy(srcPath,targetPath);
+                
+                AssetDatabase.Refresh();
             }
         }
     }
